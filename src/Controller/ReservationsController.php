4e1df19b06e5fa079fe299f5a9e12ae8a7d7810c
Gary Clarke <garyclarke1974@gmail.com>
2024-04-23 14:34:32 +0100

    Create a reservation via the flights base endpoint

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -1,15 +1,20 @@
-<?php
+<?php // src/Controller/ReservationsController.php
 
 declare(strict_types=1);
 
 namespace App\Controller;
 
 use App\Entity\EntityValidator;
+use App\Entity\Flight;
+use App\Entity\Passenger;
+use App\Entity\Reservation;
 use App\Repository\ReservationRepository;
 use App\Serializer\Serializer;
 use Doctrine\ORM\EntityManagerInterface;
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpNotFoundException;
 
 readonly class ReservationsController extends ApiController
 {
@@ -38,5 +43,54 @@ readonly class ReservationsController extends ApiController
 
         // Return a cacheable response
         return $response->withHeader('Cache-Control', 'public, max-age=600');
+    }
+
+    public function store(Request $request, Response $response, string $number): Response
+    {
+        // Grab the post data and map to a flight
+        $reservationJson = $request->getParsedBody();
+
+        // Get the passenger and the flight
+        $passenger = $this->entityManager->getRepository(Passenger::class)
+            ->findOneBy(['reference' => $reservationJson['passengerReference']]);
+
+        if (!$passenger) {
+            throw new HttpNotFoundException($request, "Passenger {$reservationJson['passengerReference']} not found.");
+        }
+
+        // Fetch Flight entity
+        $flight = $this->entityManager->getRepository(Flight::class)
+            ->findOneBy(['number' => $number]);
+
+        if (!$flight) {
+            throw new HttpNotFoundException($request, "Flight {$reservationJson['flightNumber']} not found.");
+        }
+
+        // Create new Reservation
+        $reservation = new Reservation();
+        $reference = strstr(time() . $number, '-', true);
+        $reservation->setReference($reference);
+        $reservation->setFlight($flight);
+        $reservation->setPassenger($passenger);
+        $reservation->setSeatNumber($reservationJson['seatNumber']);
+        $reservation->setTravelClass($reservationJson['travelClass']);
+
+        // Validate the reservation
+        $this->validator->validate($reservation, $request);
+
+        // Persist the new Reservation
+        $this->entityManager->persist($reservation);
+        $this->entityManager->flush();
+
+        // Serialize the new reservation
+        $jsonReservation = $this->serializer->serialize(
+            ['reservation' => $reservation]
+        );
+
+        // Add the reservation to the response body
+        $response->getBody()->write($jsonReservation);
+
+        // Return the response
+        return $response->withStatus(StatusCodeInterface::STATUS_CREATED);
     }
 }
